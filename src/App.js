8139dcb089652066bd4e8c04aae5b5c41f108805
Marcin Zimecki <marcin.zimecki@lhsystems.com>
2020-05-04 09:00:17 +0200

    loading spinner added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import * as React from "react";
 import Table from "./Table";
 import "semantic-ui-css/semantic.min.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from 'react-loader-spinner'
 
 function App() {
   const [covidData, setCovidData] = React.useState({});
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch("https://api.thevirustracker.com/free-api?countryTimeline=PL")
       .then((res) => res.json())
-      .then((res) => setCovidData(res));
+      .then((res) => {
+        setCovidData(res);
+        setIsLoading(false);
+      })
+      .catch(error => console.log(error));
   }, []);
 
   return (
-    <div>
-      <Table covidData={covidData} />
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+        {
+          isLoading ? 
+          <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} /> :  
+          <Table covidData={covidData} />
+        }
     </div>
   );
 }
--- src/Table.js
+++ src/Table.js
@@ -1,44 +1,40 @@
 import * as React from "react";
 
 function Table({ covidData }) {
-  if (covidData !== null && covidData.hasOwnProperty("timelineitems")) {
     return (
-      <div>
-        <h1>
-          COVID-19 statistics for: {covidData.countrytimelinedata[0].info.title}
-        </h1>
-        <h3>source: {covidData.countrytimelinedata[0].info.source}</h3>
-        <table className="ui celled table">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>New daily cases</th>
-              <th>New daily deaths</th>
-              <th>Totatl cases</th>
-              <th>Total recoveries</th>
-              <th>Total deaths</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(covidData.timelineitems[0]).reverse().filter((key) => key !== "stat").map((key) => {
-                const data = covidData.timelineitems[0];
-                return (
-                  <tr key={key}>
-                    <td>{key}</td>
-                    <td>{data[key].new_daily_cases}</td>
-                    <td>{data[key].new_daily_deaths}</td>
-                    <td>{data[key].total_cases}</td>
-                    <td>{data[key].total_recoveries}</td>
-                    <td>{data[key].total_deaths}</td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-      </div>
-    );
-  } else {
-    return <h2>Loading data...</h2>;
-  }
+    <div>
+      <h1>
+        COVID-19 statistics for: {covidData.countrytimelinedata[0].info.title}
+      </h1>
+      <h5>source: {covidData.countrytimelinedata[0].info.source}</h5>
+      <table className="ui celled table">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>New daily cases</th>
+            <th>New daily deaths</th>
+            <th>Total cases</th>
+            <th>Total recoveries</th>
+            <th>Total deaths</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(covidData.timelineitems[0]).reverse().filter((key) => key !== "stat").map((key) => {
+              const data = covidData.timelineitems[0];
+              return (
+                <tr key={key}>
+                  <td>{key}</td>
+                  <td>{data[key].new_daily_cases}</td>
+                  <td>{data[key].new_daily_deaths}</td>
+                  <td>{data[key].total_cases}</td>
+                  <td>{data[key].total_recoveries}</td>
+                  <td>{data[key].total_deaths}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 export default Table;

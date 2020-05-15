e9aafc10fbbc0211ae8bd0b866b9f0e3eb79b110
Marcin Zimecki <marcin.zimecki@lhsystems.com>
2020-05-15 09:54:35 +0200

    map of Poland added

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,6 @@
 import * as React from "react";
+import MapOfPoland from "./MapOfPoland";
+import 'leaflet/dist/leaflet.css';
 
 function Header({totalData}) {
     
@@ -32,56 +34,66 @@ function Header({totalData}) {
                         COVID-19 statistics for: {totalCovidData.countrydata[0].info.title}
                         <div className="sub header">Data source: {totalCovidData.countrydata[0].info.source}</div>
                     </h1>
-                    <div className="ui statistics">
-                        <div className="red statistic">
-                            <div className="value">
-                            {totalCovidData.countrydata[0].total_cases}
-                            </div>
-                            <div className="label">
-                            Total Cases
+                    <div className="ui stackable two column grid">
+                        <div className="column">
+                            <div className="ui statistics">
+                                <div className="red statistic">
+                                    <div className="value">
+                                    {totalCovidData.countrydata[0].total_cases}
+                                    </div>
+                                    <div className="label">
+                                    Total Cases
+                                    </div>
+                                </div>
+                                <div className="green statistic">
+                                    <div className="value">
+                                    {totalCovidData.countrydata[0].total_recovered}
+                                    </div>
+                                    <div className="label">
+                                    Total Recoveries
+                                    </div>
+                                </div>
+                                <div className="black statistic">
+                                    <div className="value">
+                                    {totalCovidData.countrydata[0].total_deaths}
+                                    </div>
+                                    <div className="label">
+                                    Total Deaths
+                                    </div>
+                                </div>
+                                <div className="red statistic">
+                                    <div className="value">
+                                    {totalCovidData.countrydata[0].total_new_cases_today}
+                                    </div>
+                                    <div className="label">
+                                    Total New Cases Today
+                                    </div>
+                                </div>
+                                <div className="black statistic">
+                                    <div className="value">
+                                    {totalCovidData.countrydata[0].total_new_deaths_today}
+                                    </div>
+                                    <div className="label">
+                                    Total New Deaths Today
+                                    </div>
+                                </div>
+                                <div className="grey statistic">
+                                    <div className="value">
+                                    {totalCovidData.countrydata[0].total_serious_cases}
+                                    </div>
+                                    <div className="label">
+                                    Serious Cases
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="green statistic">
-                            <div className="value">
-                            {totalCovidData.countrydata[0].total_recovered}
-                            </div>
-                            <div className="label">
-                            Total Recoveries
-                            </div>
-                        </div>
-                        <div className="black statistic">
-                            <div className="value">
-                            {totalCovidData.countrydata[0].total_deaths}
-                            </div>
-                            <div className="label">
-                            Total Deaths
-                            </div>
-                        </div>
-                        <div className="red statistic">
-                            <div className="value">
-                            {totalCovidData.countrydata[0].total_new_cases_today}
-                            </div>
-                            <div className="label">
-                            Total New Cases Today
-                            </div>
-                        </div>
-                        <div className="black statistic">
-                            <div className="value">
-                            {totalCovidData.countrydata[0].total_new_deaths_today}
-                            </div>
-                            <div className="label">
-                            Total New Deaths Today
-                            </div>
-                        </div>
-                        <div className="grey statistic">
-                            <div className="value">
-                            {totalCovidData.countrydata[0].total_serious_cases}
-                            </div>
-                            <div className="label">
-                            Serious Cases
+                        <div className="column">
+                            <div id="container">
+                                <MapOfPoland/>
                             </div>
                         </div>
                     </div>
+                    
                 </div>
             }
         </div>
---
+++ src/MapOfPoland.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+function MapOfPoland() {
+    const DefaultIcon = L.icon({
+        iconUrl: icon,
+        shadowUrl: iconShadow
+    });
+    
+    L.Marker.prototype.options.icon = DefaultIcon;
+    const position = [52.54, 18.70]
+    return (
+        <Map center={position} zoom={6}>
+            <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup>
+                    Poland.
+                </Popup>
+            </Marker>
+        </Map>
+    )
+}
+
+export default MapOfPoland;
